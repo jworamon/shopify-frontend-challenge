ae6f4251b7c638d58d5fbe80135156c3e73700e7
Woramon Jamjod <jworamon@gmail.com>
2022-05-16 16:31:47 -0400

    mvp frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import react, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import getResponseFromOpenAI from '../resources';
 import Responses from './Responses';
--- src/components/Responses.js
+++ src/components/Responses.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import ResponseCard from "./ResponseCard";
 
 const Responses = (props) => {
